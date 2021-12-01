913ff4545ef1d5e49ae77930ed7e1cf553dc3dd2
tcosse <tcosse@student.42.fr>
2021-12-01 17:35:05 +0100

    nice

--- day00/ex02/tests.cpp
+++ day00/ex02/tests.cpp
@@ -15,46 +15,45 @@
 
 int		main( void ) {
 
-	typedef std::vector<Account::t>							  accounts_t;
-	typedef std::vector<int>								  ints_t;
-	typedef std::pair<accounts_t::iterator, ints_t::iterator> acc_int_t;
+	typedef std::vector<Account::t>						accounts_t;
+	typedef std::vector<int>						ints_t;
+	typedef std::pair<accounts_t::iterator, ints_t::iterator> 		acc_int_t;
 
-	int	const				amounts[]	= { 42, 54, 957, 432, 1234, 0, 754, 16576 };
-	size_t const			amounts_size( sizeof(amounts) / sizeof(int) );
-	accounts_t				accounts( amounts, amounts + amounts_size );
-	accounts_t::iterator	acc_begin	= accounts.begin();
-	accounts_t::iterator	acc_end		= accounts.end();
+	int	const		amounts[] = { 42, 54, 957, 432, 1234, 0, 754, 16576 }; //tab de int
+	size_t const		amounts_size( sizeof(amounts) / sizeof(int) ); //taille de la tab de int
+	accounts_t		accounts( amounts, amounts + amounts_size ); //construction d'un vecteur account : var(1er element, dernier_elem);
+	accounts_t::iterator	acc_begin = accounts.begin(); //iterrateur pour parcourir le vecteur accounts --> accounts.begin()
+	accounts_t::iterator	acc_end	= accounts.end(); //element apres le derniere element du vecteur
 
-	int	const			d[]			= { 5, 765, 564, 2, 87, 23, 9, 20 };
+	int const		d[] = { 5, 765, 564, 2, 87, 23, 9, 20 };
 	size_t const		d_size( sizeof(d) / sizeof(int) );
-	ints_t				deposits( d, d + d_size );
-	ints_t::iterator	dep_begin	= deposits.begin();
-	ints_t::iterator	dep_end		= deposits.end();
-
-	int	const			w[]			= { 321, 34, 657, 4, 76, 275, 657, 7654 };
+	ints_t			deposits( d, d + d_size ); //creation d'uin vecteur contenant des int
+	ints_t::iterator	dep_begin = deposits.begin(); //premiere element
+	ints_t::iterator	dep_end	= deposits.end(); //element apres la fin
+	//pareil que dessus
+	int	const		w[] = { 321, 34, 657, 4, 76, 275, 657, 7654 };
 	size_t const		w_size( sizeof(w) / sizeof(int) );
-	ints_t				withdrawals( w, w + w_size );
-	ints_t::iterator	wit_begin	= withdrawals.begin();
-	ints_t::iterator	wit_end		= withdrawals.end();
+	ints_t			withdrawals( w, w + w_size );
+	ints_t::iterator	wit_begin = withdrawals.begin();
+	ints_t::iterator	wit_end	= withdrawals.end();
+	Account::displayAccountsInfos(); //[time_stamp] accounts:nb_account;total:somme_ammounts;deposits:nb_deposit;withdrawals:nb_witdrawals
+	std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) ); //pour chaque element de faire account::displaystatus : [time_stamp] index:0;amount:42;deposits:0;withdrawals:0
 
-	Account::displayAccountsInfos();
-	std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) );
-
-	for ( acc_int_t it( acc_begin, dep_begin );
+	for ( acc_int_t it( acc_begin, dep_begin ); //pair de accounts et des depots qui leur seront fait
 		  it.first != acc_end && it.second != dep_end;
-		  ++(it.first), ++(it.second) ) {
+		  ++(it.first), ++(it.second) ) { //pour chaque element de la pair account/deposit first->account second->deposite;
 
-		(*(it.first)).makeDeposit( *(it.second) );
+		(*(it.first)).makeDeposit( *(it.second) ); //on depose dans account un element du vecteur de int deposite
 	}
 
-	Account::displayAccountsInfos();
+	Account::displayAccountsInfos(); 
 	std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) );
 
 	for ( acc_int_t it( acc_begin, wit_begin );
 		  it.first != acc_end && it.second != wit_end;
 		  ++(it.first), ++(it.second) ) {
 
-		(*(it.first)).makeWithdrawal( *(it.second) );
+		(*(it.first)).makeWithdrawal( *(it.second) ); //meme principe que l' autre for mais avec withdrawal
 	}
 
 	Account::displayAccountsInfos();
