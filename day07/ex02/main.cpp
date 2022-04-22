6452d5191e7ff12c4ddd8d1377003ef3434d73a5
Theo COSSE <tcosse@e3r11p12.clusters.42paris.fr>
2022-04-22 15:57:36 +0200

    rendu

--- day07/ex02/main.cpp
+++ day07/ex02/main.cpp
@@ -1,9 +1,55 @@
 #include <iostream>
-#include <string>
-#include "Array.hpp"
+#include <Array.hpp>
 
+#define MAX_VAL 750
+int main(int, char**)
+{
+    Array<int> numbers(MAX_VAL);
+    int* mirror = new int[MAX_VAL];
+    srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const int value = rand();
+        numbers[i] = value;
+        mirror[i] = value;
+    }
+    //SCOPE
+    {
+        Array<int> tmp = numbers;
+        Array<int> test(tmp);
+    }
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != numbers[i])
+        {
+            std::cerr << "didn't save the same value!!" << std::endl;
+            return 1;
+        }
+    }
+    try
+    {
+        numbers[-2] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        numbers[MAX_VAL] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 
-int	main(){
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        numbers[i] = rand();
+    }
+    delete [] mirror;//
+	std::cout << "======================================================mon main=========================================" << std::endl;
+	{
 	Array<int> a(5);
 	std::string	tabs[2] = {
 		"bat",
@@ -31,5 +77,6 @@ int	main(){
 		std::cout << e.what() << std::endl;
 	}
 	delete cpy;
-	return 0;
+	}
+    return 0;
 }
